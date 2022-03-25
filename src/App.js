789cbc879b6b7abdb25e5d2d78ec302e25e5a498
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2022-03-24 21:37:37 -0500

    Se hizo el diseño de la card

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Home } from './Components/Pages/Home/Home';
 
 function App() {
   return (
-    <div>
+    <div >
       <Home/>
     </div>
   );
--- src/Components/UI/Card/Card.jsx
+++ src/Components/UI/Card/Card.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Card.css'
 export const Card = () => {
   return (
-    <div>Card</div>
+    <div className='contentCard'>
+      <img src="https://cdn.pixabay.com/photo/2022/01/17/19/59/dog-6945696_960_720.jpg" alt=""  className='cardImage'/>
+      <h2 className='text_rosa'>Perrito _kun</h2>
+    </div>
   )
 }
