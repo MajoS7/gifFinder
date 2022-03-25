a8e8b7748ab1647756ade2ec366bdc54f806c9db
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2022-03-24 23:13:37 -0500

    se organizo la distorción de los gifs

--- src/Components/UI/Card/Card.jsx
+++ src/Components/UI/Card/Card.jsx
@@ -3,10 +3,7 @@ import './Card.css'
 export const Card = ({titleCard,imageCard}) => {
   return (
     <div className='contentCard'>
-      <div className='contentImage'>
-        <img src={imageCard} alt={titleCard}  className='cardImage'/>
-      </div>
-      
+      <img src={imageCard} alt={titleCard}  className='cardImage'/>
       <h2 className='text_rosa'>{titleCard}</h2>
     </div>
   )
